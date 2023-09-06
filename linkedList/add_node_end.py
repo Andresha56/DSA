31d15267c1c3aba700ebe8032f30535c897a05f4
Rinki <91174884+Andresha56@users.noreply.github.com>
2023-09-06 16:12:44 +0530

    add node at end of linked list

---
+++ linkedList/add_node_end.py
@@ -0,0 +1,41 @@
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.ref = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    # ---------- Traversing a linked----------
+
+    def printLL(self):
+        if self.head is None:
+          print("Linked list is empty")
+        else:
+            current=self.head
+            while  current is not None:
+                print(current.data, " " , " => " , " " ,end="")
+                current=current.ref
+        
+        
+    # ------add element--at --begin---
+
+    def add_element_at_begin(self, data):
+        node = Node(data)
+        node.ref = self.head
+        self.head = node
+
+    # -------at element--at--end---------
+
+    def add_element_at_end(self,data):
+        node=Node(data)
+        if(self.head == None):
+            self.head=node
+        else :
+            current= self.head
+            while(current.ref is not None):
+                current=current.ref
+            current.ref=node
+        
